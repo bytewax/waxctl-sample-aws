6c0922b02fb16fee83a2999da8c570caeac64b1b
awmatheson <a.w.matheson@gmail.com>
2022-10-14 12:43:37 -0400

    remove test

--- test_dataflow.py
+++ test_dataflow.py
@@ -7,13 +7,15 @@ def test_dataflow():
     out = []
     flow.capture(TestingOutputConfig(out))
     run_main(flow)
-    data = [('commons.wikimedia.org', 1),
-('ca.wikipedia.org', 1),
-('species.wikimedia.org', 1),
-('en.wikipedia.org', 2),
-('ar.wikipedia.org', 1),
-('fr.wikipedia.org', 1),
-('id.wikipedia.org', 9),
-('www.wikidata.org', 15),
-('ro.wikipedia.org', 1),]
+    data = [
+            ('commons.wikimedia.org', 1),
+            ('ca.wikipedia.org', 1),
+            ('species.wikimedia.org', 1),
+            ('en.wikipedia.org', 2),
+            ('ar.wikipedia.org', 1),
+            ('fr.wikipedia.org', 1),
+            ('id.wikipedia.org', 9),
+            ('www.wikidata.org', 15),
+            ('ro.wikipedia.org', 1),
+        ]
     assert sorted(out) == sorted(data)
